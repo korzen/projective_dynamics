1332caf934b410162b84303bdf52cf43f527cacd
Will Usher <willusher.life@gmail.com>
2016-04-20 10:48:46 -0600

    Add some simple shading

--- data/fs.glsl
+++ data/fs.glsl
@@ -10,7 +10,12 @@ in vec4 v_pos;
 void
 main(void)
 {
+        const vec3 light_dir = normalize(vec3(1, 0.5, 1));
         vec3 n = normalize(cross(dFdx(v_pos.xyz), dFdy(v_pos.xyz)));
-        color = vec4(0.5*n + 0.5, 1.0);
-//        color = vec4(1 - gl_FragCoord.z);
+        float geom_term = dot(n, light_dir);
+        if (geom_term <= 0.0){
+                color = vec4(0.2);
+        } else {
+                color = vec4(0.2) + vec4(0.7) * geom_term;
+        }
 }
